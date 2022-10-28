5d76c4aad0e72933eb43b78d6528578d9e1d9667
Julius Sladek <julo.sladek@gmail.com>
2022-10-28 16:52:56 +0200

    finished hash, probably

--- biaks2/podpis.cpp
+++ biaks2/podpis.cpp
@@ -9,44 +9,22 @@ using namespace std;
 
 string myHash(string data) {
 
-	string hashedMsg;
-
+	string cryptedMsg = "";
+	string hashedMsg = "";
+	
 	for (int i = 0; i < data.length(); ++i)
-		hashedMsg += (int(data[i]) + SHIFT);
+		cryptedMsg += (int(data[i]) + SHIFT);
+
+	for (int i = cryptedMsg.size() - 1; i > cryptedMsg.size() - 33; --i)
+		hashedMsg += bitset<8>(cryptedMsg.c_str()[i]).to_string();
 
 	return hashedMsg;
 }
 
-//string myUnhash(string data) {      // SPRAVU NETREBA ODHASHOVAT KEDZE SA POSIELA NEHESHOVANA A HASH STACI POROVNAT
-//
-//	string unHashedMsg;
-//
-//	for (int i = 0; i < data.length(); ++i)
-//		unHashedMsg += (int(data[i]) - SHIFT);
-//
-//	return unHashedMsg;
-//}
-
 int main(void)
 {	
-	string temp;
 	string msg = "toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava,toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava";
-	
-	/*temp = myHash(msg);
-	cout << "hashed msg: " << temp << endl << endl;
-
-	temp = myUnhash(temp);
-	cout << "hashed msg: " << temp << endl << endl;*/
-
-
-	string mojString = "";
-	for (int i = msg.size() - 1; i > msg.size() - 33; --i)
-	{
-		//cout << bitset<8>(msg.c_str()[i]) << endl;   VYPIS
-		mojString += bitset<8>(msg.c_str()[i]).to_string();
-	}
-	
-	cout << endl << "moje bitiky: " << mojString;
+	cout << "Hashed msg: " << myHash(msg) << endl << endl;
 
 	return 0;
 }
