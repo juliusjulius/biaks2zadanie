88a4449a9400d8633df647e2029860f30fcba125
Julius Sladek <julo.sladek@gmail.com>
2022-10-28 19:45:46 +0200

    implement prime number generator

--- biaks2/podpis.cpp
+++ biaks2/podpis.cpp
@@ -2,10 +2,18 @@
 #include <string> 
 #include <vector>
 #include <bitset>
-
-const int SHIFT = 5;
+#include <boost/multiprecision/miller_rabin.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/independent_bits.hpp>
+// https://www.numberempire.com/primenumbers.php prime number checker
 
 using namespace std;
+using namespace boost::multiprecision;
+using namespace boost::random;
+
+const int SHIFT = 5;
+cpp_int privateKey;
+cpp_int publicKey;
 
 string myHash(string data) {
 
@@ -21,10 +29,34 @@ string myHash(string data) {
 	return hashedMsg;
 }
 
+cpp_int getPrime()
+{
+	mt11213b base_gen(clock());
+	boost::random::independent_bits_engine<mt11213b, 512, cpp_int> gen(base_gen);
+
+	cpp_int n = 1;
+	do
+	{
+		n = gen();
+	} while (!miller_rabin_test(n, 25));
+	return n;
+}
+
 int main(void)
 {	
 	string msg = "toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava,toto je tajna sprava, toto je tajna sprava, toto je tajna sprava, toto je tajna sprava";
-	cout << "Hashed msg: " << myHash(msg) << endl << endl;
+	//cout << "Hashed msg: " << myHash(msg) << endl << endl;
+
+	privateKey = getPrime();
+	publicKey = getPrime();
+
+	while (privateKey == publicKey) 
+	{
+		publicKey = getPrime();
+	}
+
+	cout << "private: " << privateKey << endl << endl << endl;
+	cout << "public: " << publicKey << endl << endl << endl;
 
 	return 0;
 }
